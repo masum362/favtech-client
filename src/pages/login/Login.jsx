4fec99fd8b266f56052c57ad764049fa71a82d63
masum362 <masumahmed64077@gmail.com>
2024-06-02 22:56:39 +0600

    added register page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomBtn from '../../components/customBtn/CustomBtn'
 import { Link } from 'react-router-dom'
 import gogleLogo from '../../assets/gogle-logo.png'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 
 const Login = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <div className='flex items-center justify-center w-full min-h-screen'>
             <div className=' border border-gray-500 shadow-md shadow-gray-500 space-y-8 rounded-lg w-1/4 p-8'>
                 <h1 className='text-5xl font-bold text-center'>Login</h1>
                 <form className='flex flex-col gap-4 w-full'>
-                    <input type="text" placeholder="Email" className="input input-bordered w-full " />
-                    <input type="text" placeholder="password" className="input input-bordered  w-full" />
+
+                    <div className='w-full'>
+                        <input type="text" placeholder="Email" className="input input-bordered w-full " />
+                    </div>
+                    <div className='w-full relative'>
+                        <input type={isOpen ? "text" : "password"} placeholder="password" className="input input-bordered w-full " />
+                        {
+                            isOpen ? <span className='absolute top-3 right-3 text-2xl cursor-pointer' onClick={() => setIsOpen(!isOpen)}><FaEyeSlash /></span> : <span className='absolute top-3 right-3 text-2xl cursor-pointer' onClick={() => setIsOpen(!isOpen)}><FaEye /></span>
+                        }
+
+
+                    </div>
                     <CustomBtn text={"Login"} style={"text-lg"} />
                 </form>
                 <p className='text-lg'>Don't have an acoount?<Link to={"/register"} className='text-themePrimary font-semibold'>Register</Link></p>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomBtn from '../../components/customBtn/CustomBtn'
 import { Link } from 'react-router-dom'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-  return (
-    <div className='flex items-center justify-center w-full min-h-screen'>
-    <div className=' border border-gray-500 shadow-md shadow-gray-500 space-y-8 rounded-lg w-1/4 p-8'>
-        <h1 className='text-5xl font-bold text-center'>Register</h1>
-        <form className='flex flex-col gap-4 w-full'>
-            <input type="text" placeholder="Name" className="input input-bordered w-full " />
-            <input type="text" placeholder="Email" className="input input-bordered w-full " />
-            <input type="text" placeholder="password" className="input input-bordered w-full " />
-            <input type="text" placeholder="photoURL" className="input input-bordered  w-full" />
-            <CustomBtn text={"Login"} style={"text-lg"} />
-        </form>
-        <p className='text-lg'>already have an acoount?<Link to={"/login"} className='text-themePrimary font-semibold'>Login</Link></p>
+    const [isOpen,setIsOpen] = useState(false)
+    return (
+        <div className='flex items-center justify-center w-full min-h-screen'>
+            <div className=' border border-gray-500 shadow-md shadow-gray-500 space-y-8 rounded-lg w-1/4 p-8'>
+                <h1 className='text-5xl font-bold text-center'>Register</h1>
+                <form className='flex flex-col gap-4 w-full'>
+                    <div className='w-full'>
+                        <input type="text" placeholder="Name" className="input input-bordered w-full " />
+                    </div>
+                    <div className='w-full'>
+                        <input type="text" placeholder="Email" className="input input-bordered w-full " />
+                    </div>
+                    <div className='w-full relative'>
+                        <input type={isOpen ? "text" : "password"} placeholder="password" className="input input-bordered w-full " />
+                       {
+                        isOpen ?  <span className='absolute top-3 right-3 text-2xl cursor-pointer' onClick={()=>setIsOpen(!isOpen)}><FaEyeSlash /></span> :   <span className='absolute top-3 right-3 text-2xl cursor-pointer'  onClick={()=>setIsOpen(!isOpen)}><FaEye /></span>
+                       }
 
 
-    </div>
-</div>
-  )
+                    </div>
+                    <div className='w-full'>
+                        <input type="text" placeholder="photoURL" className="input input-bordered  w-full" />
+                    </div>
+                    <CustomBtn text={"Login"} style={"text-lg"} />
+                </form>
+                <p className='text-lg'>already have an acoount?<Link to={"/login"} className='text-themePrimary font-semibold'>Login</Link></p>
+
+
+            </div>
+        </div>
+    )
 }
 
 export default Register
