b6f0cb644b9f3c48b7d56a7f0a8a4539c97d0899
masum362 <masumahmed64077@gmail.com>
2024-06-04 11:22:22 +0600

    responsive navbar done

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -16,14 +16,24 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100 w-full fixed top-0 left-0 border-b border-gray-300 py-4 px-8 z-[999]">
-                <div className=" navbar-start">
+            <div className="navbar bg-base-100 w-full fixed top-0 left-0 border-b border-gray-300 md:py-4 md:px-8 z-[999] ">
+                <div className=" navbar-start ">
+                    <div className="dropdown md:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><NavLink className={({ isActive }) => `${isActive && 'text-themePrimary'} hover:text-themePrimary capitalize active:text-themeSecondary `} to={"/"}>home</NavLink></li>
+                            <li><NavLink className={({ isActive }) => `${isActive && 'text-themePrimary'} hover:text-themePrimary capitalize active:text-themeSecondary `} to={"/products"}>products</NavLink></li>
+
+                        </ul>
+                    </div>
                     <div className='text-white bg-themePrimary w-10 h-10 font-bold rounded-full flex items-center justify-center'>
                         <h1 className='text-3xl' >P</h1>
-
                     </div>
+
                 </div>
-                <div className=" navbar-center">
+                <div className=" navbar-center hidden md:flex">
                     <ul className="flex items-center justify-center gap-8 px-1 text-lg font-medium">
                         <li><NavLink className={({ isActive }) => `${isActive && 'text-themePrimary'} hover:text-themePrimary capitalize active:text-themeSecondary `} to={"/"}>home</NavLink></li>
                         <li><NavLink className={({ isActive }) => `${isActive && 'text-themePrimary'} hover:text-themePrimary capitalize active:text-themeSecondary `} to={"/products"}>products</NavLink></li>
@@ -31,7 +41,7 @@ const Navbar = () => {
                     </ul>
 
                 </div>
-                <div className='navbar-end text-lg font-semibold gap-4'>
+                <div className='navbar-end text-lg font-semibold gap-4 '>
                     {
                         user ? <div className="dropdown dropdown-end">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
@@ -51,7 +61,7 @@ const Navbar = () => {
                         </div> : <>
 
                             <Link to={"/login"}><CustomBtn text={"Login"} style={"hover:text-themePrimary capitalize active:text-themeSecondary"} /></Link>
-                            <Link to={"/register"}><CustomBtn text={"Register"} style={"hover:text-themePrimary capitalize active:text-themeSecondary"} /></Link>
+                            <Link to={"/register"}><CustomBtn text={"Register"} style={"hover:text-themePrimary capitalize active:text-themeSecondary hidden sm:block"} /></Link>
                         </>
                     }
                 </div>
