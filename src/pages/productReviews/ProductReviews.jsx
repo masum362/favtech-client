4ba9dbdf9ecddf718acfd092186a51015f683cac
masum362 <masumahmed64077@gmail.com>
2024-06-08 18:17:50 +0600

    featured product is working properly

--- src/pages/productReviews/ProductReviews.jsx
+++ src/pages/productReviews/ProductReviews.jsx
@@ -4,7 +4,7 @@ import useAuthSecure from '../../hooks/useAuthSecure'
 import { FaEye } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
-
+import Swal from 'sweetalert2'
 
 const ProductReviews = () => {
   // const { user } = useAuth();
@@ -18,10 +18,45 @@ const ProductReviews = () => {
   })
 
 
-  const handleFeatured = async(productId) => {
-    const response = await authSecure.put(`/product/feature/${productId}`);
-    console.log(response);
+  const handleFeatured = async (productId) => {
+
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Feature it!"
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await authSecure.patch(`/product/feature/${productId}`);
+        if (response.status === 200) {
+          Swal.fire({
+            title: "Featured done!",
+            text: "Your product has been featured succesffully.",
+            icon: "success"
+          });
+          refetch()
+        }
+        else if (response.status === 204) {
+          Swal.fire({
+            icon: "error",
+            title: "Already Featured Product!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Something went wrong!",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+          });
+        }
+      }
+    });
   }
+  
   // const handleFeatured = async(productId) => {
   //   const response = await authSecure.put(`/product/feature/${productId}`);
   //   console.log(response);
@@ -54,15 +89,15 @@ const ProductReviews = () => {
                     <td>{product.name}</td>
                     <td ><span className={`${product.status === "pending" ? "text-black bg-yellow-500" : product.status === "accepted" ? "text-white bg-green-600" : "text-white bg-red-600"} px-4 py-1 rounded-full`}>{product.status}</span></td>
                     <td className='flex gap-2'>
-                     <div className="tooltip" data-tip="view details">
-                     <Link to={`/product/${product._id}`}><button className='btn bg-themePrimary/80 hover:bg-themePrimary text-white'><FaEye /></button></Link>
-                     </div>
-                      {!product.isFeatured && <div className="tooltip" data-tip="make featured"><button className='btn bg-yellow-600 hover:bg-yellow-800 text-white' onClick={()=>handleFeatured(product._id)}><FaRegStar /></button></div>}
+                      <div className="tooltip" data-tip="view details">
+                        <Link to={`/product/${product._id}`}><button className='btn bg-themePrimary/80 hover:bg-themePrimary text-white'><FaEye /></button></Link>
+                      </div>
+                      {!product.isFeatured && <div className="tooltip" data-tip="make featured"><button className='btn bg-yellow-600 hover:bg-yellow-800 text-white' onClick={() => handleFeatured(product._id)}><FaRegStar /></button></div>}
                       {
-                        product.status !== "accepted" && <button onClick={()=>handleAccept(product._id)} className='btn bg-green-600 text-white hover:bg-green-700'>Accept</button>
+                        product.status !== "accepted" && <button onClick={() => handleAccept(product._id)} className='btn bg-green-600 text-white hover:bg-green-700'>Accept</button>
                       }
                       {
-                        product.status !== "rejected" && <button onClick={()=>handleReject(product._id)} className='btn bg-red-600 hover:bg-red-800 text-white'>Reject</button>
+                        product.status !== "rejected" && <button onClick={() => handleReject(product._id)} className='btn bg-red-600 hover:bg-red-800 text-white'>Reject</button>
                       }
 
                     </td>
